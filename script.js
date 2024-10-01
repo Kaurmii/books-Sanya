fcbe0630233e29d2d2e72e9bf4a452677aea8248
Kaurmii <kaurmiari@gmail.com>
2024-10-01 20:41:12 +0300

    aksdklw

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 
+let themeBtn = document.querySelector("#themeToggle")
+
+
+function setTheme(theme) {
+    if (theme == 'light') {
+        document.body.classList.add("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-moon"></i>';
+    } else {
+        document.body.classList.remove("light-theme");
+        themeBtn.innerHTML = '<i class="bi bi-brightness-high"></i>';
+    }
+}
+
+
+let theme = getCookieValue('theme')
+setTheme(theme)
+
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle('light-theme'); // Перемикаємо клас теми
+    if (theme == 'light') {
+        theme = 'dark'
+    } else {
+        theme = 'light'
+    }
+    setTheme(theme)
+    // Зберігаємо JSON рядок у кукі
+    document.cookie = `theme=${theme}; max-age=${60 * 60 * 24 * 7}; path=/`;
+})
+
+
+// Очікуємо завантаження сторінки
+document.addEventListener('DOMContentLoaded', function() {
+    // Отримуємо всі написи для анімації
+    const textElements = document.querySelectorAll('.fade-in-text');
+
+
+    // Додаємо клас "show" для запуску анімації
+    textElements.forEach(element => {
+        element.classList.add('show');
+    });
+});
+
+
+
